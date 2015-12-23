bcba8fee2ef0cf256eb0183120f5b407963e0039
lucas-rodrigues20 <lucas.rodrigues20@outlook.com>
2015-12-23 15:09:37 -0200

    Criando o projeto e configurando o Spring

---
+++ src/main/java/com/whatido/conf/AppWebConfiguration.java
@@ -0,0 +1,25 @@
+package com.whatido.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.whatido.controllers.HomeController;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses={HomeController.class})
+public class AppWebConfiguration {
+	
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		
+		resolver.setExposedContextBeanNames("carrinhoCompras");
+		
+		return resolver;
+	}
+
+}

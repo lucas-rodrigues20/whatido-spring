a9e17ccc8107dfd3406421272ce4e702a9f64fd9
lucas-rodrigues20 <lucas.rodrigues20@outlook.com>
2016-01-20 14:32:34 -0200

    Fechamento automatico das mensagens

--- src/main/webapp/resources/js/site.js
+++ src/main/webapp/resources/js/site.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
 	$(".checkFinalizar").change(function(){
 		$(this).closest('form').submit();
 	});
+	
+	window.setTimeout(function() {
+		$(".alert-dismissible").alert('close');
+	}, 4000);
 });
